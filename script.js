ff088d954aeee5cb6375d5a87a7bfc3c01060e91
chinnu313 <srinivasghanta4123@gmail.com>
2024-10-23 18:20:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+  const d = new Date();
+  const htime = d.getHours();
+  const mtime = d.getMinutes();
+  const stime = d.getSeconds();
+
+  const hrotation = 30 * htime + mtime / 2;
+  const mrotation = 6 * mtime;
+  const srotation = 6 * stime;
+
+  hours.style.transform = `rotate(${hrotation}deg)`;
+  minutes.style.transform = `rotate(${mrotation}deg)`;
+  seconds.style.transform = `rotate(${srotation}deg)`;
+
+  const formattedTime = `${htime.toString().padStart(2, '0')}:${mtime.toString().padStart(2, '0')}:${stime.toString().padStart(2, '0')}`;
+  document.getElementById('digital-clock').innerText = formattedTime;
+}, 1000);
